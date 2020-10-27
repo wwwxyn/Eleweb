bd0d3f419dfabc21903f56880b1e85af8e59d997
Wxxyn <zhn95330@126.com>
2020-10-27 12:03:28 +0800

    我的崩溃在他妈窗外淋碎

--- src/main/java/vip/epss/dao/GoodsMapper.java
+++ src/main/java/vip/epss/dao/GoodsMapper.java
@@ -11,7 +11,6 @@ public interface GoodsMapper {
     int deleteByExample(GoodsExample example);
 
     int deleteByPrimaryKey(Integer gid);
-
     int insert(Goods record);
 
     int insertSelective(Goods record);
